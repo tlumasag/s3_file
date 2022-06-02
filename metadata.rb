2d79a9d92dcd26404ee1d12396724c640eb96fac
Troy Lumasag <tlumasag@gmail.com>
2022-06-02 15:40:12 +1000

    fix responds_to
    fix responds_to

--- metadata.rb
+++ metadata.rb
@@ -6,10 +6,10 @@ description      "Installs/Configures s3_file LWRP"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "2.9.0"
 
-source_url 'https://github.com/adamsb6/s3_file' if responds_to(:source_url)
-issues_url 'https://github.com/adamsb6/s3_file/issues' if responds_to(:issues_url)
+source_url 'https://github.com/adamsb6/s3_file' if respond_to(:source_url)
+issues_url 'https://github.com/adamsb6/s3_file/issues' if respond_to(:issues_url)
 
-chef_version '>= 12.6' if responds_to(:chef_version)
+chef_version '>= 12.6' if respond_to(:chef_version)
 
 supports 'centos'
 supports 'redhat'
