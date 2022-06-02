3012af51843eb157d51f339c31b59ecfdbb0f3e3
Troy Lumasag <tlumasag@gmail.com>
2022-06-02 15:56:58 +1000

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,11 +6,6 @@ description      "Installs/Configures s3_file LWRP"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "2.9.0"
 
-source_url 'https://github.com/adamsb6/s3_file' if respond_to(:source_url)
-issues_url 'https://github.com/adamsb6/s3_file/issues' if respond_to(:issues_url)
-
-chef_version '>= 12.6' if respond_to(:chef_version)
-
 supports 'centos'
 supports 'redhat'
 supports 'windows'
